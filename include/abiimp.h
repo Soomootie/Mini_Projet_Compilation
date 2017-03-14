ade146aaab803274481a50162b12dcec63807fdd
Soomootie <clement.boutet@etu.u-bordeaux.fr>
2017-03-14 08:47:42 +0100

    FIX includes

--- include/abiimp.h
+++ include/abiimp.h
@@ -1,6 +1,7 @@
 #ifndef __ABIIMP__
 #define __ABIIMP__
-
+#include "environ.h"
+#include "bilquad.h"
 typedef struct Noeud Noeud;
 struct Noeud{
   char *data;
@@ -10,5 +11,7 @@ struct Noeud{
 
 Noeud *create_noeud(Noeud *, Noeud *, char *);
 void print_tree(Noeud *);
+int env_tree(Noeud *, ENV *);
+void env_c3a(BILQUAD);
 
 #endif
